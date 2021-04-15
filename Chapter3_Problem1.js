3120396e28ffa27229940d9670dd7579bb1f3b46
trungjs <tnghiem99@gmail>
2021-04-14 22:09:31 -0700

    Solution to Ch3 P1

---
+++ Chapter3_Problem1.js
@@ -0,0 +1,14 @@
+//Chapter 3 Problem 1 Minimum
+/*The previous chapter introduced the standard function Math.min that returns its smallest argument. 
+We can build something like that now. Write a function min that takes two arguments and returns their minimum.*/
+
+let min = function(num1,num2) {
+    if(num1 == num2) return num1;
+    else if(num1 < num2) return num1;
+    else return num2;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
