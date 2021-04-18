4169b15f4e6cc6d6f7779095c83c864df57bcec1
trungjs <tnghiem99@gmail>
2021-04-18 10:36:57 -0700

    Solution to CH4P2

---
+++ CH4P2-ReversingAnArray.js
@@ -0,0 +1,17 @@
+function reverseArray(arr){
+    let rArr=[];
+    for(i=0;i<arr.length;i++) {
+        rArr.unshift(arr[i]);
+    }
+    return rArr;
+}
+
+reverseArrayInPlace = (arr) => {
+    for(i=0;i<Math.floor(arr.length/2);i++) {
+        let num1 = arr[i];
+        let num2 = arr[arr.length-1-i];
+        arr[i] = num2;
+        arr[arr.length-1-i] = num1;
+    }
+    return arr;
+}
