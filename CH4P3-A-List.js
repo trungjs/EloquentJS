a2e79f751ebf92afd4ce51277e2c777d0208ddc6
Trung Nghiem <82557909+trungjs@users.noreply.github.com>
2021-04-19 14:16:05 -0700

    Update CH4P3-A-List.js

--- CH4P3-A-List.js
+++ CH4P3-A-List.js
@@ -43,5 +43,5 @@ function nth(list,pos) {
 function nth(list,pos) {
     if (!list) return undefined;
     else if (pos == 0) return list.value;
-    else return nth(list.rest, pos-1);
+    else return nth(list.rest, pos - 1);
 }
